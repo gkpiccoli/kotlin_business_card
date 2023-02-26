9e04de9e1ccbe63da107ebc8ab989ea308cf87d2
Gustavo Kureck Piccoli <gustavokpiccoli@gmail.com>
2023-02-26 10:26:04 -0300

    feat: fun image

--- app/src/main/java/com/example/businesscard/MainActivity.kt
+++ app/src/main/java/com/example/businesscard/MainActivity.kt
@@ -3,12 +3,18 @@ package com.example.businesscard
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.businesscard.ui.theme.BusinessCardTheme
@@ -29,6 +35,19 @@ class MainActivity : ComponentActivity() {
         }
     }
 }
+@Composable
+fun CardWithAndroidLogo() {
+    val image = painterResource(id = R.drawable.android_logo)
+    Box {
+        Image(painter = image, contentDescription = null,
+        modifier = Modifier
+            .fillMaxWidth()
+            .fillMaxHeight(),
+        contentScale = ContentScale.Crop)
+
+    }
+}
+
 
 @Composable
 fun PersonalData(name: String, title: String) {
