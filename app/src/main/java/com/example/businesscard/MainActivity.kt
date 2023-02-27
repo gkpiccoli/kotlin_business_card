0150bf5638874734608b6089293e837844d6b1d3
Gustavo Kureck Piccoli <gustavokpiccoli@gmail.com>
2023-02-27 10:50:26 -0300

    working

--- app/src/main/java/com/example/businesscard/MainActivity.kt
+++ app/src/main/java/com/example/businesscard/MainActivity.kt
@@ -4,19 +4,19 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.businesscard.ui.theme.BusinessCardTheme
 
 class MainActivity : ComponentActivity() {
@@ -35,25 +35,40 @@ class MainActivity : ComponentActivity() {
         }
     }
 }
+
 @Composable
-fun CardWithAndroidLogo() {
-    val image = painterResource(id = R.drawable.android_logo)
-    Box {
-        Image(painter = image, contentDescription = null,
+fun PersonalData(name: String, title: String) {
+    Column {
+        Text(text = "$name $title", fontSize = 32.sp,
         modifier = Modifier
             .fillMaxWidth()
-            .fillMaxHeight(),
-        contentScale = ContentScale.Crop)
+            .wrapContentWidth(Alignment.CenterHorizontally)
+            .padding(all = 20.dp))
 
+
+
+
+        Text(text = title, fontSize = 24.sp, modifier = Modifier
+        .fillMaxWidth()
+            .wrapContentWidth(Alignment.CenterHorizontally)
+            .padding(start = 30.dp, top = 20.dp, end = 1.dp))
     }
 }
 
 
 @Composable
-fun PersonalData(name: String, title: String) {
-    Text(text = "Hello $name! You are $title")
-}
+fun AndroidLogo() {
+    val image = painterResource(id = R.drawable.android_logo)
+    Box {
+        Image(painter = image, contentDescription = null,
+            modifier = Modifier
+                .fillMaxWidth()
+                .fillMaxHeight(),
+            contentScale = ContentScale.Crop)
 
+    }
+
+}
 @Preview(showBackground = true,
     name = "My preview",
     backgroundColor = 0x94481a83,
@@ -63,6 +78,6 @@ fun PersonalData(name: String, title: String) {
 @Composable
 fun DefaultPreview() {
     BusinessCardTheme {
-        PersonalData("Android", title = "A Good Guy")
+        PersonalData("Gustavo K. Piccoli", title = "Android Developer Extraordinaire")
     }
 }
